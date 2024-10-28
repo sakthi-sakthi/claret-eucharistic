a5c2d5856d3fb0cb6ebffff6f4011b79cbdc30ae
Sakthi Ganapathi <110651198+sakthi-sakthi@users.noreply.github.com>
2024-10-28 23:24:51 +0530

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use App\Http\Controllers\Auth\ApiController;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
+// api.php
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
